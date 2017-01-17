313dc7d2ce32790c1b35d03c25858fb37de79890
iceluke <iceluke@users.noreply.github.com>
2017-01-17 23:10:08 +0000

    added args for update script, full path

--- update_list.sh
+++ update_list.sh
@@ -1,24 +1,46 @@
-applicationPath="/Users/lucagiuliani/Documents/UCL/COMP105P (Robots)/Simulator/Client/applications/"
-cmakeListPath="/Users/lucagiuliani/Documents/UCL/COMP105P (Robots)/Simulator/Client/applications/CMakeLists.txt"
-directory="my_apps/"
+# Use getopts (Adrian Frühwirth) 
+# 	http://stackoverflow.com/questions/16483119/example-of-how-to-use-getopts-in-bash
+# Bash arguments (Juergen Haas)
+# 	https://www.lifewire.com/pass-arguments-to-bash-script-2200571
+# -z 
+# 	http://stackoverflow.com/questions/18096670/what-does-z-mean-in-bash
 
-#depending on your machine but avoids an error message
+usage() { echo "Usage : $0 [-a <string : application path>] [-d <string : apps directory path>]" 1>&2;exit 1; }
+
+while getopts ":a:d:" opt; do
+	case "${opt}" in
+		a) applicationPath="${OPTARG}/" ;;
+		d) directory="${OPTARG}/" ;; 
+		*) usage ;;
+	esac
+done
+
+shift $((OPTIND-1))
+
+# check if paths are null
+if [ -z "${applicationPath}" ] || [ -z "${directory}" ]; then
+    usage
+fi
+
+cmakeListPath="${applicationPath}CMakeLists.txt"
+
+# depending on your machine but avoids an error message
 echo "cmake_minimum_required(VERSION 3.5)" > "${cmakeListPath}"
 
-#the one that was already here
+# the one that was already here
 echo "DECLARE_APP(spinLeftRight spinLeftRight.c)" >> "${cmakeListPath}"
 
-#get the paths and append to the file
+# get the paths and append to the file
 for i in $(ls -d */); do
 	for f in ${i}* ; do
-		path=$directory$f
+		echo $f
+		path=${directory}${f}
 		name=${f%%.*}
 		name=${name#*/}
-		echo "DECLARE_APP(${name} ${path})" >> "${cmakeListPath}"
+		echo "DECLARE_APP(${name} \"${path}\")" >> "${cmakeListPath}"
 	done
 done
 
-#this considers that your apps directory is in the simulator directory
-cd "$applicationPath"
+cd "${applicationPath}"
 cmake .
 make
